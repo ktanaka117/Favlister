fda0781feb489a34d507cf8b2c22bc90409c8cc0
田中賢治 <key.m4096.kung.fu@gmail.com>
2016-02-07 18:29:14 +0900

    BondとQuickを追加

--- Podfile
+++ Podfile
@@ -7,4 +7,8 @@ pod 'Alamofire', '~> 3.0'
 pod 'SDWebImage', '~> 3.7'
 pod 'SwiftyJSON', :git => 'https://github.com/SwiftyJSON/SwiftyJSON.git'
 
+pod 'Bond', '~> 4.0'
+
+pod 'Quick', '~> 0.9.0'
+pod 'Nimble', '3.0.0'
 
--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -85,11 +85,17 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/Alamofire.framework"
+  install_framework "Pods/Bond.framework"
+  install_framework "Pods/Nimble.framework"
+  install_framework "Pods/Quick.framework"
   install_framework "Pods/SDWebImage.framework"
   install_framework "Pods/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/Alamofire.framework"
+  install_framework "Pods/Bond.framework"
+  install_framework "Pods/Nimble.framework"
+  install_framework "Pods/Quick.framework"
   install_framework "Pods/SDWebImage.framework"
   install_framework "Pods/SwiftyJSON.framework"
 fi
