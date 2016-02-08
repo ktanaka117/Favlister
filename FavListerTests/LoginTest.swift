5fdf8bb2ef9dcbd0dbef7d4038a0a08bb50fc7a6
田中賢治 <key.m4096.kung.fu@gmail.com>
2016-02-08 14:41:14 +0900

    Loginのテストを仮実装

--- FavListerTests/LoginTest.swift
+++ FavListerTests/LoginTest.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+import Nimble
+import Quick
+
+class LoginTest: QuickSpec {
+    override func spec() {
+        describe("ログインした時にユーザーネームktanaka117の文字列を取得できる") {
+            context("成功") {
+                it("'成功!!!'と出力") {
+//                    expect(Login.login).to(equal("ktanaka117"))
+                }
+            }
+        }
+    }
+}
